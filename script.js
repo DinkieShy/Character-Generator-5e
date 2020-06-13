9de3f6f493e6a6b7d13f50c3d1d3ff01fc3399c3
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2020-06-13 16:08:28 +0100

    Mobile (ish) and stats
    Stats + media query

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-
-RACES = ["Aarakocra","Aasimar","Bugbear","Centaur","Changling","Dragonborn","Dwarf","Elf","Firbolg","Genasi",
+RACES = ["Aarakocra","Aasimar","Bugbear","Centaur","Changeling","Dragonborn","Dwarf","Elf","Firbolg","Genasi",
 	"Githyanki","Githzerai","Gnome","Goblin","Goliath","Half Elf","Half Orc","Halfling","Human","Hobgoblin",
 	"Kalashtar","Kenku","Kobold","Lizardfolk","Loxodon","Locathah","Minotaur","Orc","Shifter","Simic Hybrid",
 	"Tabaxi","Tiefling","Tortle","Triton","Vedalken","Verdan","Warforged","Yuan-ti"]
@@ -20,11 +18,26 @@ function getClass(){
 	return CLASSES[getRandomInt(CLASSES.length)]
 }
 
+function generateStat(){
+	rolls = []
+	for(var i = 0; i < 4; i++){
+		rolls.push(getRandomInt(6)+1);
+	}
+	rolls.sort()
+	console.log(rolls);
+	return rolls[1]+rolls[2]+rolls[3]
+}
+
+stats = ["DEX", "STR", "CON", "INT", "WIS", "CHA"]
+
 $(function(){
 	console.log("init");
 	$('#generateCharacter').click(function(){
 		$('#raceDisp')[0].innerHTML = getRace();
 		$('#classDisp')[0].innerHTML = getClass();
-		console.log(`${getRace()} ${getClass()}`);
+
+		for(var i = 0; i < stats.length; i++){
+			$("#" + stats[i])[0].innerHTML = generateStat();
+		}
 	});
 });
