d045161b87a2cd89b233ba2e55657664933739d1
DinkieShy <32346447+DinkieShy@users.noreply.github.com>
2021-02-20 20:13:04 +0000

    Stat bonuses and swap DEX/STR

--- script.js
+++ script.js
@@ -1,7 +1,83 @@
-RACES = ["Aarakocra","Aasimar","Bugbear","Centaur","Changeling","Dragonborn","Dwarf","Elf","Firbolg","Genasi",
-	"Githyanki","Githzerai","Gnome","Goblin","Goliath","Half Elf","Half Orc","Halfling","Human","Hobgoblin",
-	"Kalashtar","Kenku","Kobold","Lizardfolk","Loxodon","Locathah","Minotaur","Orc","Shifter","Simic Hybrid",
-	"Tabaxi","Tiefling","Tortle","Triton","Vedalken","Verdan","Warforged","Yuan-ti"]
+RACES = 				[ "Aarakocra",
+									"Aasimar",
+									"Bugbear",
+									"Centaur",
+									"Changeling",
+									"Dragonborn",
+									"Dwarf",
+									"Elf",
+									"Firbolg",
+									"Genasi",
+									"Githyanki",
+									"Githzerai",
+									"Gnome",
+									"Goblin",
+									"Goliath",
+									"Half Elf",
+									"Half Orc",
+									"Halfling",
+									"Human",
+									"Hobgoblin",
+									"Kalashtar",
+									"Kenku",
+									"Kobold",
+									"Lizardfolk",
+									"Loxodon",
+									"Locathah",
+									"Minotaur",
+									"Orc",
+									"Shifter",
+									"Simic Hybrid",
+									"Tabaxi",
+									"Tiefling",
+									"Tortle",
+									"Triton",
+									"Vedalken",
+									"Verdan",
+									"Warforged",
+									"Yuan-ti"
+								]
+
+STAT_CHANGES = 	[
+								 [0, 2, 0, 0, 1, 0],
+								 [0, 0, 0, 0, 0, 2],
+								 [2, 1, 0, 0, 0, 0],
+								 [2, 0, 0, 0, 1, 0],
+								 [0, 1, 0, 0, 0, 1],
+								 [2, 0, 0, 0, 0, 1],
+								 [0, 0, 2, 0, 0, 0],
+								 [0, 2, 0, 0, 0, 0],
+								 [1, 0, 0, 0, 2, 0],
+								 [0, 0, 2, 0, 0, 0],
+								 [2, 0, 0, 1, 0, 0],
+								 [2, 0, 0, 0, 2, 0],
+								 [0, 0, 0, 2, 0, 0],
+								 [0, 2, 1, 0, 0, 0],
+								 [2, 0, 1, 0, 0, 0],
+								 [0, 0, 0, 0, 0 ,2],
+								 [2, 0, 1, 0, 0, 0],
+								 [0, 2, 0, 0, 0, 0],
+								 [1, 1, 1, 1, 1, 1],
+								 [0, 0, 2, 1, 0, 0],
+								 [0, 0, 0, 0, 2, 1],
+								 [0, 2, 0, 0, 1, 0],
+								 [-2, 2, 0, 0, 0, 0],
+								 [0, 0, 2, 0, 1, 0],
+								 [0, 0, 2, 0, 1, 0],
+								 [2, 1, 0, 0, 0, 0],
+								 [1, 0, 0, 0, 0, 0],
+								 [2, 0, 1, -2, 0, 0],
+								 [0, 1, 0, 0, 0, 0],
+								 [0, 0, 2, 0, 0, 0],
+								 [0, 2, 0, 0, 0, 1],
+								 [0, 0, 0, 0, 0, 2],
+								 [2, 0, 0, 0, 1, 0],
+								 [1, 0, 1, 0, 0, 1],
+								 [0, 0, 0, 2, 1, 0],
+								 [0, 0, 1, 0, 0, 2],
+								 [1, 0, 1, 0, 0, 0],
+								 [0, 0, 0, 1, 0, 2]
+								]
 
 CLASSES = ["Barbarian","Bard","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock",
 	"Wizard","Artificer","Blood Hunter"]
@@ -28,14 +104,17 @@ function generateStat(){
 	return rolls[1]+rolls[2]+rolls[3]
 }
 
-stats = ["DEX", "STR", "CON", "INT", "WIS", "CHA"]
+stats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"]
 
 function updateDisplay(){
-	$('#raceDisp')[0].innerHTML = getRace();
+	race = getRace()
+	$('#raceDisp')[0].innerHTML = race;
 	$('#classDisp')[0].innerHTML = getClass();
 
 	for(var i = 0; i < stats.length; i++){
-		$("#" + stats[i])[0].innerHTML = generateStat();
+		newStat = generateStat();
+		newStat += STAT_CHANGES[RACES.indexOf(race)][i]
+		$("#" + stats[i])[0].innerHTML = newStat;
 	}
 }
 
